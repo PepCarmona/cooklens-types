88e41ec5d7fbd5fb0d238fb4c27d20b1b2a10af6
PepCarmona <pep.carmona.coll@gmail.com>
2021-12-17 16:44:17 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 13,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'warn',
+			'tab'
+		],
+		'linebreak-style': [
+			'warn',
+			'windows'
+		],
+		'quotes': [
+			'warn',
+			'single'
+		],
+		'semi': [
+			'warn',
+			'always'
+		]
+	}
+};
