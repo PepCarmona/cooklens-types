2a00bd9a5f58a90b69f7dff768c51a6ecd8a30b3
PepCarmona <pep.carmona.coll@gmail.com>
2022-02-08 20:38:31 +0100

    1.1.5

--- src/MealPlanTypes.ts
+++ src/MealPlanTypes.ts
@@ -1,20 +1,20 @@
-import { Recipe } from './RecipeTypes';
+import { Recipe } from "./RecipeTypes";
 
-export const meals = ['breakfast', 'lunch', 'snack', 'dinner'] as const;
+export const meals = ["breakfast", "lunch", "snack", "dinner"] as const;
 export type Meal = typeof meals[number];
 
 export interface DayMeal {
-	meal?: Meal;
-	recipe?: Recipe;
+  meal?: Meal;
+  recipe?: Recipe;
 }
 
 export interface DayPlan {
-	date: string;
-	meals: DayMeal[];
+  date: string;
+  meals: DayMeal[];
 }
 
 export interface MealPlan {
-	_id?: string;
-	users: string[];
-	days: DayPlan[];
+  _id?: string;
+  users: string[];
+  days: DayPlan[];
 }
