ea91d241843b6e0e362a62cc2deac06d77a80482
PepCarmona <pep.carmona.coll@gmail.com>
2022-03-27 12:07:54 +0200

    fix(recipe interface): import correct interface to MealPlan

--- src/MealPlanTypes.ts
+++ src/MealPlanTypes.ts
@@ -1,20 +1,20 @@
-import { Recipe } from "./RecipeTypes";
+import { DatabaseRecipe } from './RecipeTypes';
 
-export const meals = ["breakfast", "lunch", "snack", "dinner"] as const;
+export const meals = ['breakfast', 'lunch', 'snack', 'dinner'] as const;
 export type Meal = typeof meals[number];
 
 export interface DayMeal {
-  meal?: Meal;
-  recipe?: Recipe;
+	meal?: Meal;
+	recipe?: DatabaseRecipe;
 }
 
 export interface DayPlan {
-  date: string;
-  meals: DayMeal[];
+	date: string;
+	meals: DayMeal[];
 }
 
 export interface MealPlan {
-  _id?: string;
-  users: string[];
-  days: DayPlan[];
+	_id?: string;
+	users: string[];
+	days: DayPlan[];
 }
